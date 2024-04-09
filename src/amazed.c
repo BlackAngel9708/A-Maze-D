101d53cb8f688109e0224052b68193eb4d7bb639
Valiance <lucas.ludovic@epitech.eu>
2024-04-09 13:55:47 +0200

    [UPDATE] Adding the algorithme to the main function

--- src/amazed.c
+++ src/amazed.c
@@ -5,6 +5,7 @@
 ** amazed
 */
 
+#include "algorithme/bfs.h"
 #include "retrieve_infos.h"
 #include "my_macros.h"
 #include "parser.h"
@@ -17,10 +18,13 @@
 int amazed(void)
 {
     map_t *map = create_map();
+    shortest_path_t *shortest_path = NULL;
     info_t *info = malloc(sizeof(info_t));
     char **instruction = parse_map(map, info);
 
     if (instruction == NULL)
+        return EPITECH_FAILURE;
+    if (get_shortest_path(map, info->end_name, &shortest_path) == FAILURE)
         return EPITECH_FAILURE;
     return SUCCESS;
 }
