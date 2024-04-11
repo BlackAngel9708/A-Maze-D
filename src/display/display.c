1379aba8fe9cec676e61924654d18ad86678d253
Valiance <lucas.ludovic@epitech.eu>
2024-04-12 00:44:58 +0200

    [UPDATE] The display doesn't end after the first robot has reached the end room

--- src/display/display.c
+++ src/display/display.c
@@ -11,38 +11,35 @@
 
 int display_shortest_path(shortest_path_t *shortest_path, info_t *info)
 {
-    shortest_path_t *robot_room = NULL;
+    shortest_path_t *robot_rooms[info->nb_robots + 1];
     int robot_finished = 0;
+    int line_print = 0;
 
-    while (shortest_path != NULL) {
-        if (shortest_path->room == NULL)
-            return FAILURE;
-        if (my_strcmp(shortest_path->room->name, info->start_name) == 0) {
-            shortest_path = shortest_path->next;
-            break;
-        }
-        shortest_path = shortest_path->next;
-    }
-    if (shortest_path == NULL || shortest_path->room == NULL || info == NULL)
-        return FAILURE;
-    while (robot_finished < info->nb_robots && shortest_path != NULL) {
-        robot_room = shortest_path;
-        for (int current_robot = robot_finished + 1; current_robot <= info->nb_rooms; current_robot += 1) {
-            if (current_robot > robot_finished + 1)
-                my_putchar(' ');
-            my_putstr("P");
-            my_put_nbr(current_robot);
-            my_putstr("-");
-            my_putstr(robot_room->room->name);
-            if (robot_room->previous == NULL
-                || my_strcmp(robot_room->previous->room->name, info->start_name) == 0) {
-                    break;
+    for (int i = 0; i <= info->nb_robots; i += 1)
+        robot_rooms[i] = NULL;
+    robot_rooms[0] = shortest_path;
+    while (robot_finished < info->nb_robots) {
+        for (int current_robot = 0; current_robot < info->nb_robots; current_robot += 1) {
+            if (robot_rooms[current_robot] == NULL)
+                continue;
+            if (my_strcmp(robot_rooms[current_robot]->room->name, info->start_name) != 0) {
+                if (line_print == TRUE)
+                    my_putchar(' ');
+                line_print = TRUE;
+                my_putstr("P");
+                my_put_nbr(current_robot + 1);
+                my_putstr("-");
+                my_putstr(robot_rooms[current_robot]->room->name);
+                robot_rooms[current_robot + 1] = robot_rooms[current_robot]->previous;
             }
-            robot_room = robot_room->previous;
+            if (my_strcmp(robot_rooms[current_robot]->room->name, info->end_name) == 0) {
+                robot_finished += 1;
+            }
+            robot_rooms[current_robot] = robot_rooms[current_robot]->next;
         }
-        my_putchar('\n');
-        robot_finished += 1;
-        shortest_path = shortest_path->next;
+        if (line_print == TRUE)
+            my_putchar('\n');
+        line_print = FALSE;
     }
     return SUCCESS;
 }
