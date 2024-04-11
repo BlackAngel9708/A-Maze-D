055ebf54dc7e3631bf83e66fc60598f3f25d34e8
Valiance <lucas.ludovic@epitech.eu>
2024-04-11 11:46:32 +0200

    [UPDATE] Release memory for all structuresy

--- src/amazed.c
+++ src/amazed.c
@@ -18,6 +18,69 @@
 #include <stdlib.h>
 
 static
+void destroy_map(map_t *map)
+{
+    if (map == NULL)
+        return;
+    destroy_map(map->next);
+    if (map->name != NULL)
+        free(map->name);
+    free(map);
+}
+
+static
+void destroy_shortest_path(shortest_path_t *shortest_path)
+{
+    if (shortest_path == NULL)
+        return;
+    destroy_shortest_path(shortest_path->next);
+    free(shortest_path);
+}
+
+static
+void destroy_info(info_t *info)
+{
+    if (info == NULL)
+        return;
+    if (info->end_name != NULL)
+        free(info->end_name);
+    if (info->start_name != NULL)
+        free(info->start_name);
+    if (info->rooms_name != NULL) {
+        for (size_t i = 0; info->rooms_name[i] != NULL; i += 1) {
+            free(info->rooms_name[i]);
+        }
+        free(info->rooms_name);
+    }
+    free(info);
+}
+
+static
+void destroy_instructions(char **instructions)
+{
+    if (instructions == NULL)
+        return;
+    for (size_t i = 0; instructions[i] != NULL; i += 1) {
+        free(instructions[i]);
+    }
+    free(instructions);
+}
+
+static
+void destroy_end(map_t *map, shortest_path_t *shortest_path,
+    info_t *info, char **instructions)
+{
+    if (map != NULL)
+        destroy_map(map);
+    if (shortest_path != NULL)
+        destroy_shortest_path(shortest_path);
+    if (info != NULL)
+        destroy_info(info);
+    if (instructions != NULL)
+        destroy_instructions(instructions);
+}
+
+static
 int display_shortest_path(shortest_path_t *shortest_path, int nb_robots)
 {
     if (shortest_path == NULL || shortest_path->room == NULL)
@@ -50,5 +113,6 @@ int amazed(void)
         return EPITECH_FAILURE;
     my_putstr("#moves\n");
     display_shortest_path(shortest_path, info->nb_robots);
+    destroy_end(map, shortest_path, info, instruction);
     return SUCCESS;
 }
